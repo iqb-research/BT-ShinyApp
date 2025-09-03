eef9e8e8c5680529e0008aef5990094577da8f19
Alina-Apel <apelalin@hu-berlin.de>
2025-09-03 16:25:09 +0200

    bugfix with run_app()

--- R/run_app.R
+++ R/run_app.R
@@ -1,6 +1,6 @@
 #' Run the Shiny App
 #' @export
 run_app <- function() {
-  appDir <- system.file("app", package = "BTShinyApp")
+  appDir <- system.file("BT_Shiny_App", package = "BTShinyApp")
   shiny::runApp(appDir, display.mode = "normal")
 }
