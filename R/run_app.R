f0e3aa52a166ce0aeb4e8db7b6956ba65ca30aec
sachseka <karoline.sachse@hu-berlin.de>
2025-09-26 15:33:57 +0200

    correct host and port

--- R/run_app.R
+++ R/run_app.R
@@ -3,5 +3,5 @@
 run_app <- function() {
   app_dir <- system.file("BT_Shiny_App", package = "BTShinyApp")
   if (app_dir == "") stop("Could not find app directory inside package.")
-  shiny::runApp(app_dir, display.mode = "normal")
+  shiny::runApp(appDir, host = "0.0.0.0", port = 3838, launch.browser = FALSE)
 }
